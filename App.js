cfd4a6a6477675b6a233e94af3fd15c18bb02dbc
Md. Kawsar Hossain <kawsarm104@gmail.com>
2021-12-29 11:38:03 +0600

    created a textinput

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Native .. </Text>
+      <Text>Writing 2nd line in react native</Text>
+      < TextInput
+         style={styles.input}
+          placeholder="Type anything here"
+       />
+
+   
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius:15
   },
 });
