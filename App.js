6a5bf3e45a7687d5429e240d34913b3fc7fe5c35
Md. Kawsar Hossain <kawsarm104@gmail.com>
2021-12-29 11:59:29 +0600

    style input field and added a button

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-web';
+import { Button, TextInput } from 'react-native-web';
 
 export default function App() {
   const handleInputChange = text =>{
@@ -8,13 +8,17 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Text>Hello Native .. </Text>
-      <Text>Writing 2nd line in react native</Text>
-      < TextInput
+      <View style={styles.inputView}>
+          < TextInput
          style={styles.input}
           placeholder="Type anything here"
           onChangeText={text=>handleInputChange(text)}
        />
+       <Button title="Add">
+
+       </Button>
+      </View>
+    
 
    
       <StatusBar style="auto" />
@@ -27,13 +31,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    flexDirection:"column"
   },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    borderRadius:15
+    borderRadius:15,
+    
   },
+  inputView:{
+    marginTop:50,
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center"
+  }
 });
